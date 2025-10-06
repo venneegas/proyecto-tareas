3cf9f3c4b0ad9efdd591ef41fe0aed00af4c3f34
eddvenegas <edgardhuayhuavenegas2@gmail.com>
2025-10-05 20:21:19 -0500

    Agregar función para añadir nuevas tareas

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ tareas.forEach(t => {
   li.textContent = t;
   lista.appendChild(li);
 });
+
+const input = document.getElementById("nuevaTarea");
+const boton = document.getElementById("btnAgregar");
+
+boton.addEventListener("click", () => {
+  const texto = input.value.trim();
+  if (texto) {
+    const li = document.createElement("li");
+    li.textContent = texto;
+    lista.appendChild(li);
+    input.value = "";
+  }
+});
